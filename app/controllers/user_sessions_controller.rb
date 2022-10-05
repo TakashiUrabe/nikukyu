104af3e6d1a7079e92d9e6c73af37fd7470519e6
TakashiUrabe <pkkjq801@yahoo.co.jp>
2022-10-05 11:57:39 +0900

    feat:ログイン機能を実装

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,24 @@
+class UserSessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password], params[:remember])
+    if @user
+      redirect_back_or_to root_path, success:'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to login_path, success:'ログアウトしました'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :remember)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to '#'
+      redirect_to login_path
+      flash[:notice] = 'ユーザーの作成に成功しました'
     else
+      flash.now[:alert] = 'ユーザーの作成に失敗しました'
       render :new
     end
   end
