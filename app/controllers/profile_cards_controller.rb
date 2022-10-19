b5435aaec439f0de2165d0d3c7cce913dbcce6f4
TakashiUrabe <pkkjq801@yahoo.co.jp>
2022-10-19 09:38:40 +0900

    fix:profile_cardsテーブルからkindを消去して修正

--- app/controllers/profile_cards_controller.rb
+++ app/controllers/profile_cards_controller.rb
@@ -2,9 +2,10 @@ class ProfileCardsController < ApplicationController
 
   def create
     @profile_card = ProfileCard.new(profile_card_params)
+    cookies[:kind] = params[:profile_card][:kind]
     if @profile_card.save
       cookies[:id] = @profile_card.id
-      @profile_card.personality = @profile_card.image_recognition(@profile_card.pad_image.url)
+      @profile_card.personality = @profile_card.image_recognition(@profile_card.pad_image.url,cookies[:kind])
       @profile_card.save
       redirect_to @profile_card, success: '診断結果です'
     else
@@ -19,25 +20,18 @@ class ProfileCardsController < ApplicationController
   end
 
   def update
-    if @profile.update(profile_card_params)
+    @profile_card = ProfileCard.find(params[:id])
+    if @profile_card.update(profile_card_params)
       redirect_to @profile_card
     else
       flash.now[:danger] = '入力項目が受け付けられませんでした'
       render :show, status: :unprocessable_entity
     end
   end
-    def update
-    if @board.update(board_params)
-      redirect_to @board, success: t('defaults.message.updated',item:Board.model_name.human)
-    else
-      flash.now[:danger] = t('defaults.message.not_updated',item:Board.model_name.human)
-      render :edit
-    end
-  end
 
   private
 
   def profile_card_params
-    params.require(:profile_card).permit(:name, :kind, :pad_image, :pad_image_cache)
+    params.require(:profile_card).permit(:name, :pad_image, :pad_image_cache)
   end
 end
