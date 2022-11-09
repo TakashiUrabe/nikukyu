7a97f426c8f0fed43216d07f395def8688728854
TakashiUrabe <pkkjq801@yahoo.co.jp>
2022-11-09 21:32:30 +0900

    feat:topページにみんなのプロフィールカードを表示

--- app/controllers/welcome_pages_controller.rb
+++ app/controllers/welcome_pages_controller.rb
@@ -1,5 +1,6 @@
 class WelcomePagesController < ApplicationController
   def top
     @profile_card = ProfileCard.new
+    @profile_cards = ProfileCard.where.not(card_type: nil).order(updated_at: :desc)
   end
 end
