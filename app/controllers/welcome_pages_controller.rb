3e6ef8126fa57738773834e1862489f0779ea977
TakashiUrabe <pkkjq801@yahoo.co.jp>
2022-11-10 09:24:17 +0900

    fix:ページネーションを5枚ごとに変更

--- app/controllers/welcome_pages_controller.rb
+++ app/controllers/welcome_pages_controller.rb
@@ -2,6 +2,6 @@ class WelcomePagesController < ApplicationController
   def top
     @profile_card = ProfileCard.new
     @profile_cards = ProfileCard.where.not(card_type: nil).order(updated_at: :desc)
-    @profile_cards = @profile_cards.page(params[:page]).per(1)
+    @profile_cards = @profile_cards.page(params[:page]).per(5)
   end
 end
