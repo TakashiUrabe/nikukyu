0bee208dde44f8fdd5eed8e23913f33f90cb5f6d
TakashiUrabe <pkkjq801@yahoo.co.jp>
2022-11-11 22:50:00 +0900

    fix:トップページをアプリの説明に使うためにprofile_cards_controllerにnewアクションを追加

--- app/controllers/welcome_pages_controller.rb
+++ app/controllers/welcome_pages_controller.rb
@@ -1,7 +1,4 @@
 class WelcomePagesController < ApplicationController
   def top
-    @profile_card = ProfileCard.new
-    @profile_cards = ProfileCard.where.not(card_type: nil).order(updated_at: :desc)
-    @profile_cards = @profile_cards.page(params[:page]).per(5)
   end
 end
