9650d877208dd5a5e2d2a1909cbc7bb48f768cb4
TakashiUrabe <pkkjq801@yahoo.co.jp>
2022-11-25 01:14:24 +0900

    #{Rails.root}を1つ書き換え

--- app/models/profile_card.rb
+++ app/models/profile_card.rb
@@ -89,7 +89,7 @@ class ProfileCard < ApplicationRecord
   end
 
   def create_profile_card_a(profile_card)
-    base_img = ImageList.new("#{Rails.root}/assets/images/base_img_a.png")
+    base_img = ImageList.new("./app/assets/images/base_img_a.png")
 
     draw = Draw.new
     draw.font      = 'app/assets/fonts/nicomoji-plus_v2.ttf'
