1bd2c0420534f2d6c734218379d64197be99f266
TakashiUrabe <pkkjq801@yahoo.co.jp>
2022-11-25 14:56:15 +0900

    reloadの必要がなさそうなので消去

--- app/javascript/controllers/reload_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="reload"
-export default class extends Controller {
-  doReload() {
-  setTimeout(() => location.reload(), 7000)
-  }
-}
