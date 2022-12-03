c253a9f9963d5afb765c0439f790de68dbcb6cee
TakashiUrabe <pkkjq801@yahoo.co.jp>
2022-12-03 17:35:53 +0900

    refactor:可変長引数にアンダースコアを挿入

--- app/uploaders/pad_image_uploader.rb
+++ app/uploaders/pad_image_uploader.rb
@@ -27,7 +27,7 @@ class PadImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
+  def default_url(*_args)
     #   # For Rails 3.1+ asset pipeline compatibility:
     #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     'pad_image_sample.png'
