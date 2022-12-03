c3dd70fed7f5718e824785f691b28115a45c74bf
TakashiUrabe <pkkjq801@yahoo.co.jp>
2022-12-03 17:35:53 +0900

    refactor:可変長引数にアンダースコアを挿入

--- app/uploaders/face_image_uploader.rb
+++ app/uploaders/face_image_uploader.rb
@@ -28,7 +28,7 @@ class FaceImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
+  def default_url(*_args)
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     'face_image_sample.png'
