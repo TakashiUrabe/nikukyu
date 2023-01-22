36ce6c013a03a595479eab01a099fe14fe83809e
TakashiUrabe <pkkjq801@yahoo.co.jp>
2023-01-22 17:51:10 +0900

    feat:インスタグラム用プロフィールカードの一つ目の作成に成功

--- app/models/profile_card.rb
+++ app/models/profile_card.rb
@@ -17,10 +17,16 @@ class ProfileCard < ApplicationRecord
   mount_uploader :profile_card_data_d, ProfileCardDataUploader
   mount_uploader :profile_card_data_e, ProfileCardDataUploader
   mount_uploader :profile_card_data_f, ProfileCardDataUploader
+  mount_uploader :instagram_profile_card_data_g, ProfileCardDataUploader
+  mount_uploader :instagram_profile_card_data_h, ProfileCardDataUploader
+  mount_uploader :instagram_profile_card_data_i, ProfileCardDataUploader
+  mount_uploader :instagram_profile_card_data_j, ProfileCardDataUploader
+  mount_uploader :instagram_profile_card_data_k, ProfileCardDataUploader
+  mount_uploader :instagram_profile_card_data_l, ProfileCardDataUploader
 
   enum gender: { male: 1, female: 2 }
   enum personality: { typeA: 1, typeB: 2, typeC: 3, typeD: 4, typeE: 5, typeF: 6, typeG: 7, typeH: 8, typeI: 9, typeJ: 10 }
-  enum card_type: { hide: 1, A: 2, B: 3, C: 4, D: 5, E: 6, F: 7 }
+  enum card_type: { hide: 1, A: 2, B: 3, C: 4, D: 5, E: 6, F: 7, G: 8, H: 9, I: 10, J: 11, K: 12, L: 13 }
 
   def user_id_setting(id)
     self.user_id = id
