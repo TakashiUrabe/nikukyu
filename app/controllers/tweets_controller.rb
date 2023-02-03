f642eee89e7b2a34701dca6cf58c3e0bff712844
TakashiUrabe <pkkjq801@yahoo.co.jp>
2023-02-03 16:47:55 +0900

    fix:tweets_controllerのツイッターシェアを修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   before_action :set_profile_card
 
   def tweet
-    card_type = { 'tweet_a': 'a', 'tweet_b': 'b', 'tweet_c': 'c', 'tweet_d': 'd', 'tweet_e': 'e', 'tweet_f': 'f' }
+    card_type = { 'tweet_a': 'a', 'tweet_b': 'b', 'tweet_c': 'c', 'tweet_d': 'd', 'tweet_e': 'e', 'tweet_f': 'f', 'tweet_g': 'g', 'tweet_h': 'h', 'tweet_i': 'i', 'tweet_j': 'j', 'tweet_k': 'k', 'tweet_l': 'l', 'tweet_m': 'm', 'tweet_n': 'n', 'tweet_o': 'o', 'tweet_p': 'p', 'tweet_q': 'q', 'tweet_r': 'r' }
 
     card_type.each  do |key, value|
       render key if params[:card_type] == value
